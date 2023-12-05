53326958166b0cf19f42bbd46121fa746d68e1eb
Paola Basualdo <paoladeembalse@gmail.com>
2023-12-05 18:46:43 +0000

    corregir settings.py

--- alphafood_project/settings.py
+++ alphafood_project/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["plvn1975.pythonanywhere.com"]
 
 
+
 # Application definition
 
 CUSTOM_APPS = ["mascotas_app"
@@ -122,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 #STATICFILES_DIRS = [ BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR/"static"
+STATIC_ROOT = BASE_DIR/ "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
